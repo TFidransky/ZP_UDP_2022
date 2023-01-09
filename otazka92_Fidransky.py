cb35b884d93ede263e58c1d4c30a9c9c255a12a9
Tomas Fidransky <tom.fidransky@gmail.com>
2023-01-09 23:57:21 +0100

    Try blok u vstupu, rozděleno do více funkcí

--- otazka92_Fidransky.py
+++ otazka92_Fidransky.py
@@ -3,39 +3,44 @@ from collections import Counter
 from itertools import chain
 import os
 
-# nejdříve načte soubor s daným názvem (v základu "vstup.csv") a vrací (returns) a tiskne (print) nejvíce se vyskytující element a jeho frekvenci
-# poté to seznamy seznamů změní na jeden větší seznam (chain) a poté seřadí a pomocí Counter počítá frekvence jednotlivých prvků v "data" (náš seřazený a velký seznam)
-# most_common hledá prvek co se objevuje nejčastěji (=modus), frquency frekvenci tohoto prvku
-# print vytiskne prvek, který je v souboru nejčastěji (= modus) a jeho četnost
-# return zde není vyloženě potřeba, ale kdybychom nechtěli jen vypsat modus, ale i s ním nějak dál počítat, tak by se return hodil.
-# v posledních řádcích probíhá exekuce celé funkce (a u tohoto programu i celého programu)
+# nejdrive nacte soubor s danym nazvem (v zakladu "vstup.csv") a vraci (returns) a tiskne (print) nejvíce se vyskytující element a jeho frekvenci
+# pote to seznamy seznamu zmeni na jeden vetsi seznam (chain) a pote seradi a pomoci Counter pocita frekvence jednotlivych prvku v "data" (nas serazeny  velky seznam)
+# most_common hleda prvek co se objevuje nejcasteji (=modus), frequency frekvenci tohoto prvku
+# print vytiskne prvek, ktery je v souboru nejcasteji (= modus) a jeho cetnost
+# return zde neni vyloženě potřeba, ale kdybychom nechteli jen vypsat modus, ale i s nim nejak dal pocitat, tak by se return hodil.
+# v poslednich radcich probiha exekuce cele funkce (a u tohoto programu i celeho programu)
 
-# třída pro výjimku prázdného souboru. Chtěl jsem se vyhnout užití knihovny pandas, když není ve VS Code defaultně a musí se stáhnout
+# trida pro vyjimku prazdneho souboru. Chtel jsem se vyhnout uziti knihovny pandas, kdyz to neni defaultni knihovna a musi se stahnout
 class EmptyFileException(Exception):
     def __init__(self, message):
         self.message = message
 
-def find_most_common_element(input_file):
+def open_file(input_file):
     try:
         with open(input_file, 'r') as input_file:
             if os.stat("vstup.csv").st_size == 0:
-                raise EmptyFileException("Vstupní soubor je prázdný.")
+                raise EmptyFileException("Vstupni soubor je prazdny.")
             reader = csv.reader(input_file)
             data = [row for row in reader]
     except FileNotFoundError:
-        print("Vstupní soubor nebyl nalezen.")
+        print("Vstupni soubor nebyl nalezen.")
         exit(1)
     except EmptyFileException as EmptyError:
         print(EmptyError)
         exit(1)
+    return data
 
-    data = sorted(chain(*data))
-    c = Counter(data)
+def modus_calculation(data):
+    data_sorted = sorted(chain(*data))
+    c = Counter(data_sorted)
 
     most_common, frequency = c.most_common(1)[0]
 
     print(f"Prvek, ktery se v souboru vyskytoval nejcasteji byl: {most_common}")
     print(f"Tento prvek (=modus) se v souboru vyskytl {frequency}xkrát.")
-    return (most_common, frequency)
 
-most_common, frequency = find_most_common_element('vstup.csv')
+def main():
+    data = open_file("vstup.csv")
+    modus_calculation(data)
+
+main()
