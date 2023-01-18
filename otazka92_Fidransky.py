9e0bd813898b654c2dbcd6d753ea79eea1fa15e3
Tomas Fidransky <tom.fidransky@gmail.com>
2023-01-18 10:44:59 +0100

    + class

--- otazka92_Fidransky.py
+++ otazka92_Fidransky.py
@@ -8,38 +8,43 @@ import os
 # most_common hleda prvek co se objevuje nejcasteji (=modus), frequency hleda frekvenci tohoto prvku
 # print vytiskne prvek, ktery je v souboru nejcasteji (= modus) a jeho cetnost
 # v poslednich radcich probiha exekuce cele funkce (a u tohoto programu i celeho programu)
-
 # trida pro vyjimku prazdneho souboru. Chtel jsem se vyhnout uziti knihovny pandas, kdyz to neni defaultni knihovna a musi se stahnout
+
 class EmptyFileException(Exception):
     def __init__(self, message):
         self.message = message
 
-def open_file(input_file):
-    try:
-        with open(input_file, 'r') as input_file:
-            if os.stat("vstup.csv").st_size == 0:
-                raise EmptyFileException("Vstupni soubor je prazdny.")
-            reader = csv.reader(input_file)
-            data = [row for row in reader]
-    except FileNotFoundError:
-        print("Vstupni soubor nebyl nalezen.")
-        exit(1)
-    except EmptyFileException as EmptyError:
-        print(EmptyError)
-        exit(1)
-    return data
+class MyFileProcessor:
+    def __init__(self, input_file):
+        self.input_file = input_file
 
-def modus_calculation(data):
-    data_sorted = sorted(chain(*data))
-    c = Counter(data_sorted)
+    def open_file(self):
+        try:
+            with open(self.input_file, 'r') as input_file:
+                if os.stat(self.input_file).st_size == 0:
+                    raise EmptyFileException("Input file is empty.")
+                reader = csv.reader(input_file)
+                data = [row for row in reader]
+        except FileNotFoundError:
+            print("Input file was not found.")
+            exit(1)
+        except EmptyFileException as EmptyError:
+            print(EmptyError)
+            exit(1)
+        return data
 
-    most_common, frequency = c.most_common(1)[0]
+    def modus_calculation(self, data):
+        data_sorted = sorted(chain(*data))
+        c = Counter(data_sorted)
 
-    print(f"Prvek, ktery se v souboru vyskytoval nejcasteji byl: {most_common}")
-    print(f"Tento prvek (=modus) se v souboru vyskytl {frequency}xkrát.")
+        most_common, frequency = c.most_common(1)[0]
+
+        print(f"Prvek, ktery se v souboru vyskytoval nejcasteji byl: {most_common}")
+        print(f"Tento prvek (=modus) se v souboru vyskytl {frequency}xkrát.")
 
 def main():
-    data = open_file("vstup.csv")
-    modus_calculation(data)
+    file_processor = MyFileProcessor("vstup.csv")
+    data = file_processor.open_file()
+    file_processor.modus_calculation(data)
 
 main()
