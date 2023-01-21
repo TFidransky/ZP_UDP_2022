81e0ae60ae193294d154ea5318f446dfe7751648
Tomas Fidransky <tom.fidransky@gmail.com>
2023-01-21 13:07:38 +0100

    New control of input, new function

--- otazka4_Fidransky.py
+++ otazka4_Fidransky.py
@@ -1,10 +1,9 @@
-import re
-import os
 
 class CharFrequencyAnalyzer:
         # Constructor that prompts user for input string.
-    def __init__(self):
-        self.string = input("Input the sentence of which you want to know the frequency of each character: ")
+    def __init__(self, string):
+        self.string = string.upper()
+        self.unwanted_chars = ".,:!?_-;%ˇ´<>'(){}[]\" "
 
         # Counts the frequency of each character in the input string and prints the results in descending order (from most frequent to least frequent).
             # If the character didnt appear yet, it will give that character value 1, if it did appear prior to that appearance, it will add 1 to its current value.
@@ -12,19 +11,16 @@ class CharFrequencyAnalyzer:
             # but it would not be as universal, because different alphabets use specific symbols, such as ř or š, that are not universally used and therefore it would be hard to implement a universal counter
         # Lastly, the function returns a list of characters sorted from the most frequent to the least frequent. This list is used in the print function.
     def char_freq(self):
-        string = self.string.upper()
         char_freq = {}
-        unwanted_chars = ".,:!?_-;%ˇ´<>'(){}[] \" "
 
-
-        for char in string:
-            if char not in unwanted_chars:
+        for char in self.string:
+            if char not in self.unwanted_chars:
                 if char not in char_freq:
                     char_freq[char] = 1
                 else:
                     char_freq[char] += 1
 
-        char_freq = sorted(char_freq.items(), key=lambda x: x[1], reverse=True)
+        self.char_freq = sorted(char_freq.items(), key=lambda x: x[1], reverse=True)
 
         return char_freq
 
@@ -32,15 +28,30 @@ class CharFrequencyAnalyzer:
     def print_char_freq(self, char_freq):
         for char, freq in char_freq:
             print(f"{char}: {freq}")
-      
+        
+        # This makes sure, that users inputs something in the terminal. If he doesn't, the program will ask again, until the user enters something.
+            # If the user enters only the symbols that are in the variable "unwanted_chars", the program will again give error code and prompt user to enter something again.        
+    def get_valid_input(self):
+        string = input("Input the sentence of which you want to know the frequency of each character: ")
+        while string.strip() == "":
+            print("No input was recorded, try again.")
+            string = input("Input the sentence of which you want to know the frequency of each character: ")
+        for char in self.unwanted_chars:
+            string = string.replace(char,'')
+        if string.strip() != "":
+            return string
+        else:
+            print("Invalid input. There were only unwanted characters in your input. Try again.")
+            return self.get_valid_input()
+
       # Main function that calls the char_freq function if a valid input string is provided.
     def main(self):
-        if self.string.strip() != "":
-            char_freq = self.char_freq()
-            self.print_char_freq(char_freq)
-        else:
-            print("No input was recorded, try again.")
+        self.string = self.get_valid_input()
+        self.char_freq()
+        self.print_char_freq()
 
-  # Create an instance of the CharFrequencyAnalyzer class and call the main function
-analyzer = CharFrequencyAnalyzer()
+    # First the istance of CharFrequencyAnalyzer class is created with empty string as input. Then the main function of this instance gets called, 
+    # which controls the input and proper calling of individual functions.
+analyzer = CharFrequencyAnalyzer('')
 analyzer.main()
+
